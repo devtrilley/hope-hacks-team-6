d463a02d44b27e68cd3b649518d3c7a82b77e530
Tamsir Rilley <tamsirrilley@gmail.com>
2025-01-06 12:12:21 -0500

    Imports express, mysql2, dotenv. Sets dev port. Middleware added for server.

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
 // This files holds our Node.js/Express server
+
+// Importing NPM modules
+const express = require("express"); // express for server app
+const mysql2 = require("mysql2"); // mysql2 modules for interacting with DB
+const dotenv = require("dotenv"); // dotenv for our .dotenv
+
+// Calling express() func wich starts our server, storing it in app variable
+// app is our server. handles all requests and sends responses.
+const app = express();
+
+const PORT = 3000; // Current port for development
+
+// Middleware to automatically parse JSON data into JS. Comes before routes (Ex: app.get())
+// Without Middleware, app wouldn't understand incoming data
+app.use(express.json());
